01a03a0511914449b0ee3b7a635bc0c9a4f5b74b
bannk9245 <bannk9245@gmail.com>
2021-03-14 01:44:06 +0800

    master:简化代码

--- main.go
+++ main.go
@@ -8,22 +8,19 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("git", "fetch","--all")
+	runCmd("git", "fetch","--all")
+	runCmd("git", "checkout","main")
+}
+
+
+func runCmd(name string, args ...string) {
+	cmd := exec.Command(name, args...)
 	cmd.Dir = "d:\\branchs\\convert"
 	var stderr bytes.Buffer
 	var out bytes.Buffer
 	cmd.Stderr = &stderr
 	cmd.Stdout = &out
 	if err := cmd.Run(); err != nil {
-		log.Fatalln(err.Error(), stderr.String())
-	}
-	fmt.Println(out.String())
-	
-	checkout := exec.Command("git", "checkout","main")
-	checkout.Dir = "d:\\branchs\\convert"
-	checkout.Stderr = &stderr
-	checkout.Stdout = &out
-	if err := checkout.Run();err != nil {
 		log.Fatalln(err.Error(), stderr.String())
 	}
 	fmt.Println(out.String())
